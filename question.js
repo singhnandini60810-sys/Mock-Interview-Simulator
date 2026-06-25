5705bfdccc91cf2b860d56e68ceebde4ee3795d3
Nandini Singh <singhnandini60810@gmail.com>
2026-06-25 10:12:21 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,31 @@
+const questionBank = {
+
+frontend: [
+
+"What is HTML?",
+
+"What is CSS?",
+
+"What is JavaScript?",
+
+"What is the DOM?",
+
+"What is Flexbox?"
+
+],
+
+python: [
+
+"What is Python?",
+
+"What is a list?",
+
+"What is a tuple?",
+
+"What is a dictionary?",
+
+"What is a function?"
+
+]
+
+};
